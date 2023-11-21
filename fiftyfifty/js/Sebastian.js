6f89273c03dfd928d1ce5ca4b5dfbe52afdafbac
Visjoe <144039509+Visjoe@users.noreply.github.com>
2023-11-21 13:49:05 +0100

    Add files via upload

---
+++ fiftyfifty/js/Sebastian.js
@@ -0,0 +1,14 @@
+// Canvas
+
+rectangle("0", "0", "240", "200", "solid", "red")
+circle("50", "100", "30", "solid", "black")
+circle("120", "100", "40", "solid", "black")
+circle("190", "100", "30", "solid", "black")
+
+
+// SVG
+
+rectangleSvg("0", "0", "240", "200", "solid", "red")
+circleSvg("50", "100", "30", "solid", "black")
+circleSvg("120", "100", "40", "solid", "black")
+circleSvg("190", "100", "30", "solid", "black")
