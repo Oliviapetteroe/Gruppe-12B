d41016fe513b2235ed6aecd7b44a9006f6c339e8
Oliviapetteroe <143933346+Oliviapetteroe@users.noreply.github.com>
2023-11-21 14:54:45 +0100

    Add files via upload

---
+++ fiftyfifty/js/figures.js
@@ -0,0 +1,46 @@
+
+// Canvas funksjoner
+const rectangle = function (left, top, width, height, mode, color) {
+	const canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
+
+	ctx.fillStyle = color;
+	ctx.fillRect(left, top, width, height);
+};
+
+const circle = function (centerleft, centertop, radius, mode, color) {
+	const canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
+
+	ctx.fillStyle = color;
+	ctx.beginPath();
+	ctx.ellipse(centerleft, centertop, radius, radius, 0, 180, 180);
+	ctx.fill();
+};
+
+// SVG funksjoner
+
+const rectangleSvg = function (left, top, width, height, mode, color) {
+	const svgDoc = document.getElementById("svg");
+
+	const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+	rect.setAttributeNS(null, "x", left);
+	rect.setAttributeNS(null, "y", top);
+	rect.setAttributeNS(null, "width", width);
+	rect.setAttributeNS(null, "height", height);
+	rect.setAttributeNS(null, "fill", color);
+
+	svgDoc.appendChild(rect);
+};
+
+const circleSvg = function (centerleft, centertop, radius, mode, color) {
+	const svgDoc = document.getElementById("svg");
+
+	const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	circle.setAttributeNS(null, "cx", centerleft);
+	circle.setAttributeNS(null, "cy", centertop);
+	circle.setAttributeNS(null, "r", radius);
+	circle.setAttributeNS(null, "fill", color);
+
+	svgDoc.appendChild(circle);
+};
