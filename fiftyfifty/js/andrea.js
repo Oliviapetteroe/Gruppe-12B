7f8e5b9a85fabc2da172c26fdd5cd951f88ae87c
Andrea Vågen <144044384+andreavaagen@users.noreply.github.com>
2023-11-21 14:20:51 +0100

    Add files via upload

---
+++ fiftyfifty/js/andrea.js
@@ -0,0 +1,7 @@
+//Canvas
+circle(200, 100, 100, "solid", "blue");
+rectangle(135, 40, 125, 125, "solid", "white");
+
+//SVG
+circleSvg(200, 100, 100, "solid", "blue");
+rectangleSvg(135, 40, 125, 125, "solid", "white");
