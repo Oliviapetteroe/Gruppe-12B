dd1bc51b7dd597126cb3a2241ebe84eec2830310
Ingribolstad <144338941+Ingribolstad@users.noreply.github.com>
2023-11-21 13:46:44 +0100

    Add files via upload

---
+++ fiftyfifty/js/ingri.js
@@ -0,0 +1,26 @@
+//Canvas
+
+circle(300, 200, 50, "solid", "red");
+
+circle(50, 200, 50, "solid", "red");
+
+circle(175, 200, 75, "solid", "yellow");
+
+rectangle(350, 350, 400, 450, "solid", "pink");
+
+
+
+//SVG
+
+circleSvg(300, 200, 50, "solid", "red");
+
+circleSvg(50, 200, 50, "solid", "red");
+
+circleSvg(175, 200, 75, "solid", "yellow");
+
+rectangleSvg(320, 390, 90, 75, "solid", "pink");
+
+
+
+
+
