afa0b66e0ca116211fe8d165b749fc9537319143
lukeroberson04 <144793402+lukeroberson04@users.noreply.github.com>
2023-11-22 10:20:39 +0100

    Update Sebastian.js
    koding

--- fiftyfifty/js/Sebastian.js
+++ fiftyfifty/js/Sebastian.js
@@ -1,14 +1,14 @@
 // Canvas
 
-rectangle("0", "0", "240", "200", "solid", "red")
-circle("50", "100", "30", "solid", "black")
-circle("120", "100", "40", "solid", "black")
-circle("190", "100", "30", "solid", "black")
+rectangle("0", "0", "240", "200", "solid", "red", "canvasSebastian")
+circle("50", "100", "30", "solid", "black", "canvasSebastian")
+circle("120", "100", "40", "solid", "black", "canvasSebastian")
+circle("190", "100", "30", "solid", "black", "canvasSebastian")
 
 
 // SVG
 
-rectangleSvg("0", "0", "240", "200", "solid", "red")
-circleSvg("50", "100", "30", "solid", "black")
-circleSvg("120", "100", "40", "solid", "black")
-circleSvg("190", "100", "30", "solid", "black")
+rectangleSvg("0", "0", "240", "200", "solid", "red", "svgSebastian")
+circleSvg("50", "100", "30", "solid", "black", "svgSebastian")
+circleSvg("120", "100", "40", "solid", "black", "svgSebastian")
+circleSvg("190", "100", "30", "solid", "black", "svgSebastian")
