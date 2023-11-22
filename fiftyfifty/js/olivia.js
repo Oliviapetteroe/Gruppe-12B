1a61bba459e2a0d8a36d09ff604579288bacd7b7
Oliviapetteroe <143933346+Oliviapetteroe@users.noreply.github.com>
2023-11-22 10:16:23 +0100

    Update olivia.js

--- fiftyfifty/js/olivia.js
+++ fiftyfifty/js/olivia.js
@@ -1,28 +1,28 @@
 //Canvas
 
-circle(115, 170, 65, "solid", "grey");
+circle(115, 170, 65, "solid", "grey", "canvasOlivia");
 
-circle(230, 150, 85, "solid", "orange");
+circle(230, 150, 85, "solid", "orange", "canvasOlivia");
 
-circle(260, 250, 100, "solid", "beige");
+circle(260, 250, 100, "solid", "beige", "canvasOlivia");
 
-rectangle(320, 390, 90, 75, "solid", "black");
+rectangle(320, 390, 90, 75, "solid", "black", "canvasOlivia");
 
-rectangle(360, 170, 60, 70, "solid", "darkgrey");
+rectangle(360, 170, 60, 70, "solid", "darkgrey", "canvasOlivia");
 
-circle(175, 230, 75, "solid", "darkblue");
+circle(175, 230, 75, "solid", "darkblue", "canvasOlivia");
 
 
 //SVG
 
-circleSvg(115, 170, 65, "solid", "grey");
+circleSvg(115, 170, 65, "solid", "grey", "svgOlivia");
 
-circleSvg(230, 150, 85, "solid", "orange");
+circleSvg(230, 150, 85, "solid", "orange", "svgOlivia");
 
-circleSvg(260, 250, 100, "solid", "beige");
+circleSvg(260, 250, 100, "solid", "beige", "svgOlivia");
 
-rectangleSvg(320, 390, 90, 75, "solid", "black");
+rectangleSvg(320, 390, 90, 75, "solid", "black", "svgOlivia");
 
-rectangleSvg(360, 170, 60, 70, "solid", "darkgrey");
+rectangleSvg(360, 170, 60, 70, "solid", "darkgrey", "svgOlivia");
 
-circleSvg(175, 230, 75, "solid", "darkblue");
+circleSvg(175, 230, 75, "solid", "darkblue", "svgOlivia");
